22b09efeec5b1a153ee00d9fafe5e27791f73ecf
piccorofg <casof_1@hotmail.com>
2022-09-11 13:01:44 -0500

    Post basics props between components, change language on msg

--- my-first-app/src/components/GreetWorld.js
+++ my-first-app/src/components/GreetWorld.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function GreetWorld(props) {    
     return(
         <div>
-            <h1>HI! { props.name }, you are { props.edad } years old</h1>
+            <h1>HI! { props.name }, you are { props.age } years old</h1>
         </div>
     )
 }
